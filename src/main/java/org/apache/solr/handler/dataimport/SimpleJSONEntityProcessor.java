ae8ac431979e5cb230fec94606e060ee346f3241
Janvi Talreja <janvi.talreja@hotwaxsystems.com>
2021-08-05 12:49:35 +0530

    updated the  code for fetching data from a paginated url.

--- src/main/java/org/apache/solr/handler/dataimport/SimpleJSONEntityProcessor.java
+++ src/main/java/org/apache/solr/handler/dataimport/SimpleJSONEntityProcessor.java
@@ -17,7 +17,6 @@ public class SimpleJSONEntityProcessor extends EntityProcessorBase {
     private String url;
     private ListIterator<Map<String, Object>> rowIterator;
     private List<Map<String, Object>> result;
-    protected List<String> placeHolderVariables;
     public static final String URL = "url";
 
     public SimpleJSONEntityProcessor() {
@@ -39,21 +38,59 @@ public class SimpleJSONEntityProcessor extends EntityProcessorBase {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             Map response = objectMapper.readValue(reader, Map.class);
-            System.out.println("=response======"+response);
+            System.out.println("---------response---------"+response);
             result = (List)response.get("docs");
+            context.setSessionAttribute("count","22",Context.SCOPE_ENTITY);
+            context.setSessionAttribute("viewIndex","1",Context.SCOPE_ENTITY);
+
             rowIterator = result.listIterator();
         } catch (IOException e) {
-            System.out.println("===exception===="+e);
+            System.out.println("------exception------"+e);
         }
     }
 
     @Override
     public Map<String, Object> nextRow() {
-
+        System.out.println("----nextRow----");
         if (!rowIterator.hasNext()) {
-            return null;
+            fetchNextRow();
         }
+
+        if (rowIterator != null)
         return rowIterator.next();
+        else
+            return null;
+    }
+
+    private void fetchNextRow()
+    {
+        int count  = Integer.parseInt((String)context.getSessionAttribute("count", Context.SCOPE_ENTITY));
+        Integer viewIndex  = Integer.parseInt((String)context.getSessionAttribute("viewIndex", Context.SCOPE_ENTITY));
+
+
+        System.out.println("------fetchNextRow------");
+        if (viewIndex < count) {
+            String nextUrl = "https://dev-hc.hotwax.io/api/products?viewIndex=" + viewIndex;
+            if (nextUrl == null) {
+                throw new DataImportHandlerException(DataImportHandlerException.SEVERE, "'" + nextUrl + "' is a required attribute");
+            }
+            System.out.println("Fetching next records for" + nextUrl);
+            reader = new BufferedReader((Reader) context.getDataSource().getData(nextUrl));
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map response = objectMapper.readValue(reader, Map.class);
+                System.out.println("------fetchNextRow------");
+                result = (List) response.get("docs");
+                if (result != null)
+                    rowIterator = result.listIterator();
+            } catch (IOException e) {
+                System.out.println("------exception------" + e);
+            }
+            viewIndex = viewIndex+1;
+            context.setSessionAttribute("viewIndex",viewIndex.toString(),Context.SCOPE_ENTITY);
+        } else {
+            rowIterator = null;
+        }
     }
 
         @Override
